1f1412d741fac1122722e44664df7623fcd690df
UweStolz <uwe.stolz@protonmail.com>
2019-11-16 12:10:45 +0100

    ref: make use of types

--- src/modules/getIconLinks.ts
+++ src/modules/getIconLinks.ts
@@ -2,7 +2,8 @@ import { load } from 'cheerio';
 import url, { UrlWithStringQuery } from 'url';
 
 function hrefIsIcon(href: string): boolean {
-  return /((icon.*\.(png|jpg))|(\w+\.ico))/.test(href);
+  const regex = /((icon.*\.(png|jpg))|(\w+\.ico))/;
+  return regex.test(href);
 }
 
 function getDomainUrl(someUrl: string): string {
@@ -11,36 +12,32 @@ function getDomainUrl(someUrl: string): string {
   return url.format(parsedUrl);
 }
 
-function linkTagLinks($: any): any[] {
-  const links: any[] = [];
-  $('link').each((index: number, element: any) => {
-    const href = $(element).attr('href');
-    if (!hrefIsIcon(href)) {
-      return;
+function linkTagLinks($: CheerioStatic): string[] {
+  const links: string[] = [];
+  $('link').each((index: number, element: CheerioElement) => {
+    const href: string = $(element).attr('href');
+    if (hrefIsIcon(href)) {
+      links.push(href);
     }
-    links.push(href);
   });
   return links;
 }
 
-function metaTagLinks($: any): any[] {
-  const links: any[] = [];
-  $('meta').each((index: number, element: any) => {
-    const property = $(element).attr('property');
-    if (property !== 'og:image') {
-      return;
+function metaTagLinks($: CheerioStatic): string[] {
+  const links: string[] = [];
+  $('meta').each((index: number, element: CheerioElement) => {
+    const property: string = $(element).attr('property');
+    if (property === 'og:image') {
+      const graphImageUrl = $(element).attr('content');
+      links.push(graphImageUrl);
     }
-
-    const graphImageUrl = $(element).attr('content');
-    links.push(graphImageUrl);
   });
-
   return links;
 }
 
 export default function getIconLinks(rootUrl: string, dom: string|Buffer): string[] {
-  const $ = load(dom);
-  let iconLinks: any[] = [];
+  const $: CheerioStatic = load(dom);
+  let iconLinks: string[] = [];
 
   iconLinks = iconLinks.concat(linkTagLinks($));
   iconLinks = iconLinks.concat(metaTagLinks($));
