f510ea4fac17b429993cceb81d6e3a9fa8d1113d
UweStolz <uwe.stolz@opuscapita.com>
2019-11-20 16:38:26 +0100

    test: adjust test

--- src/util/iconLinks/getDomainUrl.test.ts
+++ src/util/iconLinks/getDomainUrl.test.ts
@@ -1,11 +1,27 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import url from 'url';
 import getDomainUrl from './getDomainUrl';
 
+const mockedUrlObject = {
+  protocol: 'https:',
+  slashes: true,
+  auth: null,
+  host: 'www.someurl.com',
+  port: null,
+  hostname: 'www.someurl.com',
+  hash: null,
+  search: null,
+  query: null,
+  pathname: '/',
+  path: '/',
+  href: 'https://www.someurl.com/',
+};
+const URL = 'https://www.SomeUrl.com/';
+
 test('Gives out the proper domain URL', () => {
-  const spyParse = jest.spyOn(url, 'parse');
+  const spyParse = jest.spyOn(url, 'parse').mockImplementationOnce((): any => mockedUrlObject);
   const spyFormat = jest.spyOn(url, 'format');
 
-  const URL = 'https://www.SomeUrl.com/';
   const result = getDomainUrl(URL);
 
   expect(spyParse).toHaveBeenCalledWith(URL);
